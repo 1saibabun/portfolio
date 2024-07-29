87a206e2a234a058d4a574c96350e07ab81d1fff
jyoti <jyotiprakash.cse@gmail.com>
2024-07-29 21:55:17 +0530

    home page design changes

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+    <nav className="navbar navbar-expand-lg navbar-light sticky-top">
       <div className="container">
         <Link className="navbar-brand" to="/">My Portfolio</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,13 +1,50 @@
 // src/pages/Home.tsx
 import React from 'react';
-import Header from '../Components/Header';
-import About from '../Components/About';
+import { Container, Row, Col } from "react-bootstrap";
+import homeLogo from "../assets/img/home-main.svg";
+import Home2 from './Home2';
+import Type from './Type';
 
 const Home = () => {
   return (
-    <div>
-      <Header />
-    </div>
+    <>    
+    <section>
+      <Container fluid className="home-section" id="home">
+        {/* <Particle /> */}
+        <Container className="home-content">
+          <Row>
+            <Col md={7} className="home-header">
+              <h1 style={{ paddingBottom: 15 }} className="heading">
+                Hi There!{" "}
+                <span className="wave" role="img" aria-labelledby="wave">
+                  👋🏻
+                </span>
+              </h1>
+
+              <h1 className="heading-name">
+                I'M
+                <strong className="main-name"> Jyoti Prakash Nayak</strong>
+              </h1>
+
+              <div style={{ padding: 50, textAlign: "left" }}>
+                <Type />
+              </div>
+            </Col>
+
+            <Col md={5} style={{ paddingBottom: 20 }}>
+              <img
+                src={homeLogo}
+                alt="home pic"
+                className="img-fluid"
+                style={{ maxHeight: "450px" }}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+      <Home2 />
+    </section>
+    </>
   );
 }
 
